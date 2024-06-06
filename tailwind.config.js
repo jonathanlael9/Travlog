c2aa73c0167ccb35c8e2ee7a27ccce9630135923
jonathanlael9 <jonathanlaelaruan@gmail.com>
2024-06-06 18:46:58 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['inter'],
+        circular: ['Circular STD'],
+      },
+      colors: {
+        primary: '#5D50C6',
+        secondary: '#F85E9F',
+        orange: '#FF5722',
+      },
+
+    },
+  },
+  plugins: [],
+};
